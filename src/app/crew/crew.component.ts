7fc38de1fa5bc47579af0b52fc52cfd5bb5d2ea1
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-12-04 19:49:13 -0600

    can edit crew members

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -10,9 +10,11 @@ export class CrewComponent implements OnInit {
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    {name: "Ellen Ochoa", firstMission: true},
   ];
 
+  memberBeingEdited: object = null;
+  
   constructor() { }
 
   ngOnInit() {
@@ -21,5 +23,19 @@ export class CrewComponent implements OnInit {
   add(memberName: string, isFirst: boolean) {
     this.crew.push({name: memberName, firstMission: isFirst});
   }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ save(name: string, member: object) {
+  member['name'] = name;
+  this.memberBeingEdited = null;
+}
 
 }
